c15d0416e6e343ba3e2319c95a65cef2ae4c57b7
Johannes Mols <johannes.mols@kraftvaerk.com>
2023-01-21 22:19:25 +0100

    Started working on project managing feature

---
+++ Infrastructure/Interfaces/IProjectService.cs
@@ -0,0 +1,15 @@
+﻿using Infrastructure.Models;
+
+namespace Infrastructure.Interfaces
+{
+    public interface IProjectService
+    {
+        Task<IEnumerable<Project>> GetAvailableProjects();
+
+        Task<Project> GetActiveProject();
+
+        Task AddProject(Project project);
+
+        Task DeleteProject(Project project);
+    }
+}
---
+++ Infrastructure/Services/ProjectService.cs
@@ -0,0 +1,38 @@
+﻿using Infrastructure.Interfaces;
+using Infrastructure.Models;
+
+namespace Infrastructure.Services
+{
+    public class ProjectService : IProjectService
+    {
+        private readonly string _projectDirectory;
+
+        public ProjectService(string projectDirectory)
+        {
+            _projectDirectory = projectDirectory;
+
+            // Ensure that the project directory exists
+            Directory.CreateDirectory(projectDirectory);
+        }
+
+        public async Task<IEnumerable<Project>> GetAvailableProjects()
+        {
+            return new List<Project>();
+        }
+
+        public async Task<Project> GetActiveProject()
+        {
+            return new Project("Test", "C:\\");
+        }
+
+        public async Task AddProject(Project project)
+        {
+            
+        }
+
+        public async Task DeleteProject(Project project)
+        {
+            
+        }
+    }
+}
