e7a74bff22185be4602af76a67fcc6cba37b4ef6
Johannes Mols <johannes.mols@googlemail.com>
2023-04-20 10:07:32 +0200

    Showing trace titles properly

--- Client/App/components/traces.svelte
+++ Client/App/components/traces.svelte
@@ -2,7 +2,8 @@
     import { Accordion, AccordionItem, ComboBox, InlineLoading, ListItem, UnorderedList } from "carbon-components-svelte";
     import { mapClient } from "../shared/pm4net-client-config";
     import { activeProject } from "../shared/stores";
-    import type { OcelValue } from "../shared/pm4net-client";
+    import type { OcelEvent, OcelObject, OcelValue, ValueTupleOfStringAndOcelEvent } from "../shared/pm4net-client";
+    import { DateTime } from "luxon";
 
     export let objectTypes : string[];
 
@@ -29,35 +30,57 @@
             return undefined;
         }*/
     }
+
+    function getTraceTitle(obj: OcelObject) {
+        let keys = Object.keys(obj.ovMap ?? {});
+        let fstObj = obj.ovMap![keys[0]];
+        if (fstObj) {
+            return getStringValue(fstObj);
+        } else {
+            return undefined;
+        }
+    }
+
+    function getDateString(obj: ValueTupleOfStringAndOcelEvent | undefined) {
+        if (obj) {
+            let lx = DateTime.fromJSDate(obj.item2.timestamp);
+            return lx.toLocaleString(DateTime.DATETIME_SHORT_WITH_SECONDS);
+        } else {
+            return "";
+        }
+    }
 </script>
 
-<Accordion>
-    <AccordionItem open>
-        <svelte:fragment slot="title"><strong>Traces</strong></svelte:fragment>
-        <ComboBox 
-            placeholder="Select an object type"
-            items={objectTypes.map(o => {return { id: o, text: o }})}
-            selectedId="0"
-            bind:value={selectedType}
-            on:select={(_) => tracesPromise = getTracesForObjectType(selectedType)}>
-        </ComboBox>
+<strong>Traces</strong>
+<ComboBox 
+    placeholder="Select an object type"
+    items={objectTypes.map(o => {return { id: o, text: o }})}
+    selectedId="0"
+    bind:value={selectedType}
+    on:select={(_) => tracesPromise = getTracesForObjectType(selectedType)}>
+</ComboBox>
 
-        {#await tracesPromise}
-            <InlineLoading description="Loading traces..."></InlineLoading>
-        {:then traces}
-            {#if traces}
-                {#each traces as t, i}
-                    <h4>Trace {i}</h4>
+{#await tracesPromise}
+    <InlineLoading description="Loading traces..."></InlineLoading>
+{:then traces}
+    {#if traces}
+        <Accordion>
+            {#each traces as t, i}
+                <AccordionItem open={false}>
+                    <svelte:fragment slot="title">
+                        <h5>{getTraceTitle(t.item1)}</h5>
+                        <div>{t.item2.length} events ({getDateString(t.item2.at(0))} - {getDateString(t.item2.at(-1))})</div>
+                    </svelte:fragment>
                     <UnorderedList>
                         {#each t.item2 as event}
                             <ListItem>{getStringValue(event.item2.vMap["pm4net_RenderedMessage"])}</ListItem>
                         {/each}
                     </UnorderedList>
-                {/each}
-            {/if}
-        {/await}
-    </AccordionItem>
-</Accordion>
+                </AccordionItem>
+            {/each}
+        </Accordion>
+    {/if}
+{/await}
 
 <style lang="scss">
 
