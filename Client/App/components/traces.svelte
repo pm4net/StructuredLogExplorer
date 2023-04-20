941bccd808fb0821b782a352f4839e5f6d53001d
Johannes Mols <johannes.mols@googlemail.com>
2023-04-20 12:05:48 +0200

    Showing trace items with arrows of duration in between

--- Client/App/components/traces.svelte
+++ Client/App/components/traces.svelte
@@ -1,13 +1,34 @@
 <script lang="ts">
-    import { Accordion, AccordionItem, ComboBox, InlineLoading, ListItem, UnorderedList } from "carbon-components-svelte";
+    import { Accordion, AccordionItem, ComboBox, ExpandableTile, InlineLoading } from "carbon-components-svelte";
     import { mapClient } from "../shared/pm4net-client-config";
     import { activeProject } from "../shared/stores";
-    import type { OcelEvent, OcelObject, OcelValue, ValueTupleOfStringAndOcelEvent } from "../shared/pm4net-client";
+    import type { OcelObject, OcelValue, ValueTupleOfStringAndOcelEvent } from "../shared/pm4net-client";
     import { DateTime } from "luxon";
+    import humanizeDuration from "humanize-duration"
 
+    // Props
     export let objectTypes : string[];
 
+    // State variables
     let selectedType : string = "";
+
+    // Humanizer
+    const shortEnglishHumanizer = humanizeDuration.humanizer({
+        language: "shortEn",
+        spacer: "",
+        languages: {
+            shortEn: {
+                y: () => "y",
+                mo: () => "mo",
+                w: () => "w",
+                d: () => "d",
+                h: () => "h",
+                m: () => "m",
+                s: () => "s",
+                ms: () => "ms",
+                },
+            },
+        });
 
     let tracesPromise = getTracesForObjectType(selectedType);
     async function getTracesForObjectType(objType: string) {
@@ -20,7 +41,7 @@
         }
     }
 
-    function getStringValue(ocelVal: OcelValue) {
+    function getStringValue(ocelVal: OcelValue) : string {
         // @ts-ignore
         return ocelVal.value;
 
@@ -46,7 +67,19 @@
             let lx = DateTime.fromJSDate(obj.item2.timestamp);
             return lx.toLocaleString(DateTime.DATETIME_SHORT_WITH_SECONDS);
         } else {
-            return "";
+            return undefined;
+        }
+    }
+
+    function getDurationBetweenTwoEvents(e1: ValueTupleOfStringAndOcelEvent | undefined, e2: ValueTupleOfStringAndOcelEvent | undefined) {
+        if (e1 && e2) {
+            let lx1 = DateTime.fromJSDate(e1.item2.timestamp);
+            let lx2 = DateTime.fromJSDate(e2.item2.timestamp);
+            let duration = lx2.diff(lx1, ['milliseconds']);
+            // Using external library until this is resolved: // https://github.com/moment/luxon/issues/1134
+            return shortEnglishHumanizer(duration.milliseconds, { units: ['y', 'mo', 'w', 'd', 'h', 'm', 's', 'ms'] });
+        } else {
+            return undefined;
         }
     }
 </script>
@@ -65,17 +98,29 @@
 {:then traces}
     {#if traces}
         <Accordion>
-            {#each traces as t, i}
+            {#each traces as t, idx_t}
                 <AccordionItem open={false}>
                     <svelte:fragment slot="title">
                         <h5>{getTraceTitle(t.item1)}</h5>
                         <div>{t.item2.length} events ({getDateString(t.item2.at(0))} - {getDateString(t.item2.at(-1))})</div>
                     </svelte:fragment>
-                    <UnorderedList>
-                        {#each t.item2 as event}
-                            <ListItem>{getStringValue(event.item2.vMap["pm4net_RenderedMessage"])}</ListItem>
-                        {/each}
-                    </UnorderedList>
+                    {#each t.item2 as event, idx_e}
+                        <ExpandableTile>
+                            <div slot="above">
+                                <strong>{getStringValue(event.item2.vMap["pm4net_RenderedMessage"])}</strong>
+                            </div>
+                            <div slot="below">
+                                
+                            </div>
+                        </ExpandableTile>
+                        {#if idx_e < t.item2.length - 1}
+                            <div class="connector">
+                                <i class="arrow down" />
+                                <p>{getDurationBetweenTwoEvents(t.item2.at(idx_e), t.item2.at(idx_e + 1))}</p>
+                                <i class="arrow down" />
+                            </div>
+                        {/if}
+                    {/each}
                 </AccordionItem>
             {/each}
         </Accordion>
@@ -83,5 +128,10 @@
 {/await}
 
 <style lang="scss">
-
+    .connector {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+    }
 </style>
