845c6880142a5b8eff9b1f8b6b95e7d1d179654e
Johannes Mols <johannes.mols@googlemail.com>
2023-05-23 22:44:53 +0200

    Started reworking selection of trace

--- Client/App/components/traces.svelte
+++ Client/App/components/traces.svelte
@@ -7,10 +7,12 @@
     import { createEventDispatcher, onMount } from "svelte";
     import { createOcDfgOptionsFromStore } from "../shared/helpers";
     import type { OcelObject, OcelValue, ValueTupleOfStringAndOcelEvent } from "../shared/pm4net-client";
+    import type { ComboBoxItem } from "carbon-components-svelte/types/ComboBox/ComboBox.svelte";
 
     // Props
     export let objectTypes : string[];
     export let selectedType = ""; // $mapSettings[$activeProject ?? ""]?.selectedTypeForTraces ?? "";
+    export let selectedObjectId = "";
 
     const dispatch = createEventDispatcher();
 
@@ -45,7 +47,7 @@
 
     function getStringValue(ocelVal: OcelValue) : string {
         // @ts-ignore
-        return ocelVal.value;
+        return ocelVal.value ?? "";
 
         /*if (ocelVal instanceof OcelString) {
             return ocelVal.value;
@@ -60,7 +62,7 @@
         if (fstObj) {
             return getStringValue(fstObj);
         } else {
-            return undefined;
+            return "";
         }
     }
 
@@ -89,20 +91,53 @@
         tracesPromise = getTracesForObjectType(selectedType);
         dispatch("highlightTraces", await tracesPromise);
     }
+
+    async function loadAndHighlightSpecificTrace() {
+
+    }
+
+    function shouldFilterItem(item: ComboBoxItem, value: string) {
+        if (!value) return true;
+        return item.text?.toLowerCase().includes(value.toLowerCase());
+    }
 </script>
 
 <ComboBox 
     placeholder="Select an object type"
-    items={objectTypes.map(o => {return { id: o, text: o }})}
+    items={objectTypes.map(o => { return { id: o, text: o } })}
     bind:value={selectedType}
-    on:clear={() => dispatch("highlightTraces", [])}
-    on:select={loadAndHighlightTraces}>
+    on:clear={() => {
+        dispatch("highlightTraces", []);
+        // Set the traces to an empty array to avoid loading element showing, but hiding previously loaded content
+        tracesPromise = Promise.resolve([]);
+    }}
+    on:select={loadAndHighlightTraces}
+    {shouldFilterItem}>
 </ComboBox>
 
 {#await tracesPromise}
     <InlineLoading description="Loading traces..."></InlineLoading>
 {:then traces}
-    {#if traces}
+    {#if traces && traces.length > 0}
+        <ComboBox
+            placeholder="Select an object identifier"
+            items={traces.map(t => { return { 
+                id: t,
+                text: getTraceTitle(t.item1) ?? ""
+            }})}
+            bind:value={selectedObjectId}
+            on:clear={() => { console.log("TODO"); }}
+            on:select={loadAndHighlightSpecificTrace}
+            {shouldFilterItem}
+            let:item>
+            <div>
+                <strong>{item.text}</strong>
+            </div>
+            <div>
+                {item.id.item2.length} events ({getDateString(item.id.item2.at(0))} - {getDateString(item.id.item2.at(-1))})
+            </div>
+        </ComboBox>
+
         <Accordion>
             {#each traces as t, idx_t}
                 <AccordionItem open={false}>
@@ -159,7 +194,15 @@
         align-items: center;
     }
 
+    :global(.bx--list-box__menu-item, .bx--list-box__menu-item__option) {
+        height: auto;
+    }
+
     :global(.bx--list-box__wrapper) {
         padding: 1rem;
+    }
+
+    :global(.bx--list-box__wrapper:nth-child(1)) {
+        padding-bottom: 0;
     }
 </style>
