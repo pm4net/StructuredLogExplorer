80ce69b99c2402853ba6bfce4b9509ec1294f83d
Johannes Mols <johannes.mols@googlemail.com>
2023-09-24 11:07:12 +0200

    Experimented a bit with d3 and manipulating dot svg

--- Client/App/components/maps/dot.svelte
+++ Client/App/components/maps/dot.svelte
@@ -3,6 +3,8 @@
     import { Graphviz } from "@hpcc-js/wasm";
     import { onMount } from "svelte";
     import panzoom from "panzoom";
+    import type { ValueTupleOfOcelObjectAndIEnumerableOfValueTupleOfStringAndOcelEvent } from "../../shared/pm4net-client";
+    import * as d3 from "d3";
 
     export let dot : string;
 
@@ -11,15 +13,38 @@
 
     onMount(() => {
         svg.then(_ => {
-            let elem = document.getElementsByTagName("g")[0];
-            if (elem) {
-                panzoom(elem, {
+            let svgElem = document.getElementsByTagName("g")[0];
+            if (svgElem) {
+                // Initialize panzoom library
+                panzoom(svgElem, {
                     maxZoom: 1,
                     initialZoom: 1
                 });
             }
+
+            // Initialize d3
+            let d3elem = d3.select("#graph0");
+            console.log("found svg", d3elem);
+
+            let nodes = d3elem.selectAll('.node');
+            console.log("found nodes", nodes);
+
+            let childNodes = nodes.filter(n => {
+                if (n instanceof HTMLElement) {
+                    console.log("is html element");
+                    return true;   
+                }
+                console.log("is not html element", typeof(n));
+                return false;
+            });
+            console.log("child", childNodes);
         });
     });
+
+    // Highlight the nodes and edges that are present in a list of traces
+    export function highlightTraces(traces: ValueTupleOfOcelObjectAndIEnumerableOfValueTupleOfStringAndOcelEvent[]) {
+        
+    }
 </script>
 
 {#await svg}
